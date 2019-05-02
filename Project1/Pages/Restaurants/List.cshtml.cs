b04ce9d714bf22f3d5c9a0f8b42f3a43ada5d0f8
RoyThompson <RoyThompson@unomaha.edu>
2019-05-01 20:03:05 -0500

    First Commit

---
+++ Project1/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+
+namespace Project1.Pages.Resturants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message{ get; set; }
+
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"]; 
+        }
+    }
+}
